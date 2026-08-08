7b1ee5fd1aaf49f4d33b2391abcb35040e0bb655
itzSumma <itzSumma11@gmail.com>
2026-08-08 22:18:30 +0600

    feat: update User model to enforce unique email and add role and orders fields

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
-import  express, { Router }  from 'express';
+import  express  from 'express';
 import cors from 'cors';
-import router from './router';
+
 
 const app = express();
  app.use(cors());
  app.use(express.json());
 
- app.use('/',Router);
  
 
  app.get("/", (req, res) => {
